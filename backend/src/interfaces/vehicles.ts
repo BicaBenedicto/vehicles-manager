8dd112c04a547bbcd52193ccdae9323b951f5035
Gabriel Benedicto <gabrielpbenedicto@gmail.com>
2022-04-04 19:58:58 -0300

    feat: adiciona tipagens dos veiculos

---
+++ backend/src/interfaces/vehicles.ts
@@ -0,0 +1,19 @@
+enum vehiclesTypes {
+  car = 'carro',
+  motorcycle = 'moto',
+}
+
+type vehiclesCategories = {
+  carro: 'Compacto' | 'Sedan' | 'SUV' | 'Caminhonete';
+  moto: 'Scooter' | 'Cidade' | 'Off - Road' | 'Sport';
+};
+
+export interface IVehicles<> {
+  type: vehiclesTypes,
+  category: vehiclesCategories,
+  brand: string,
+  model: string,
+  color: string,
+  plate: string,
+  year: number,
+};
